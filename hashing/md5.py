698102aaafcdfbe998e45bd4174bda7656e78c84
hasnainroopawalla <hasnain.roopawalla@gmail.com>
2022-09-22 12:59:24 +0530

    Hex messages and initialization vectors

--- hashing/md5.py
+++ hashing/md5.py
@@ -1,50 +1,51 @@
 from typing import List
-from utils import format_int_to_binary
+from hashing.utils import binary_string_to_hex
+from utils import format_int_to_binary_string
 from textwrap import wrap
 
 
-def text_to_binary_message(text: str):
-    return ''.join([format_int_to_binary(ord(ch), 8) for ch in text])
+A = 0x01234567
+B = 0x89ABCDEF
+C = 0xFEDCBA98
+D = 0x76543210
 
-def apply_padding(message):
+
+def text_to_binary_message(text: str) -> str:
+    return "".join([format_int_to_binary_string(ord(ch), 8) for ch in text])
+
+
+def apply_padding(message: str) -> str:
     message_length = len(message)
-    
+
     # Pad a trailing '1'
-    message += '1'
-    
+    message += "1"
+
     # Pad 0s to assert a message length of 448 bits
-    message = message.ljust(512 - 64, '0')
-    
+    message = message.ljust(512 - 64, "0")
+
     # Pad the last 64 bits that indicate the message length
-    message += format_int_to_binary(message_length, 64)
-    
+    message += format_int_to_binary_string(message_length, 64)
+
     return message
 
 
 def split_message_block(message_block: str, message_bit_count: int = 32) -> List[str]:
     return wrap(message_block, message_bit_count)
-    
-    
+
+
+def message_to_hex(message_words: List[str]) -> List[int]:
+    return [binary_string_to_hex(m) for m in message_words]
+
+
 if __name__ == "__main__":
-    text =  "They are deterministic"
-    assert len(text) <= 56 # 448/8
-    
+    text = "They are deterministic"
+    assert len(text) <= 56  # 448/8
+
     message_block = text_to_binary_message(text)
     padded_message_block = apply_padding(message_block)
-    
-    m = split_message_block(padded_message_block)
-    print(m, len(m))
-    
-    # print(hex(int(m[0], 2)))
-    
-    
-    
-    
-    # num1 = '01010100011010000110010101111001'
-    # num2 = '01010101011010000110010101001001'
-    # num1_hex = hex(int(num1, 2))
-    # num2_hex = hex(int(num2, 2))
-    
-    # print(num1_hex, num2_hex, hex(0x89abcdef & 0xfedcba98))
-    
-    
+
+    message_words = split_message_block(padded_message_block)
+
+    message_words_hex = message_to_hex(message_words)
+
+    print([[m, hex(m), int(hex(m), 16)] for m in message_words_hex])
--- hashing/utils.py
+++ hashing/utils.py
@@ -1,6 +1,12 @@
 def get_bit_count(binary: str) -> int:
-    return len(format(binary, 'b'))
+    return len(format(binary, "b"))
 
-def format_int_to_binary(integer: int, output_bit_count: int = 8) -> str:
+
+def format_int_to_binary_string(integer: int, output_bit_count: int = 8) -> str:
     assert get_bit_count(integer) <= output_bit_count
-    return str(format(integer, 'b')).zfill(output_bit_count)
+    return str(format(integer, "b")).zfill(output_bit_count)
+
+
+def binary_string_to_hex(binary: str) -> int:
+    decimal = int(binary, 2)
+    return int(hex(decimal), 16)

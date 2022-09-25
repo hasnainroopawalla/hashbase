9500c951dfbf79f18855034bb21d8532bef8bcf7
hasnainroopawalla <hasnain.roopawalla@gmail.com>
2022-09-25 10:58:12 +0530

    Working MD5, pending code cleanup

--- hashing/md5.py
+++ hashing/md5.py
@@ -1,6 +1,12 @@
 from math import floor, sin
 from typing import List
-from hashing.utils import bytearray_to_binary_string, hex_32_bit_shift_left, binary_string_to_hex, modular_addition, format_int_to_binary_string
+from hashing.utils import (
+    bytearray_to_binary_string,
+    binary_string_to_hex,
+    left_shift,
+    modular_addition,
+    format_int_to_binary_string,
+)
 from textwrap import wrap
 
 
@@ -9,10 +15,10 @@ from textwrap import wrap
 # C = 0xFEDCBA98
 # D = 0x76543210
 
-A = 0x67452301
-B = 0xEFCDAB89
-C = 0x98BADCFE
-D = 0x10325476
+A, initial_A = 0x67452301, 0x67452301
+B, initial_B = 0xEFCDAB89, 0xEFCDAB89
+C, initial_C = 0x98BADCFE, 0x98BADCFE
+D, initial_D = 0x10325476, 0x10325476
 
 
 def text_to_binary_message(text: str) -> str:
@@ -21,21 +27,28 @@ def text_to_binary_message(text: str) -> str:
 
 def apply_padding(message: bytearray) -> bytearray:
     message_length = len(message)
-    
+
     # Pad a trailing '1'
     message.append(0x80)
-    
-    # Pad 0s to assert a message length of 448 bits
-    message = message.ljust(56, b'\0')
-    
-    # Pad the last 64 bits that indicate the message length in the little endian format 
-    message += (message_length * 8).to_bytes(8, byteorder='little')
-    
+
+    # Pad 0s to assert a message length of 448 bits (56 bytes)
+    message = message.ljust(56, b"\0")
+
+    # Pad the last 64 bits that indicate the message length in the little endian format
+    message += (message_length * 8).to_bytes(8, byteorder="little")
+
     return message
 
 
-def split_message_block(message_block: str, message_bit_count: int = 32) -> List[str]:
-    return wrap(message_block, message_bit_count)
+def split_message_block(
+    message_block: bytearray, message_block_length: int = 4
+) -> List[bytearray]:
+    return [
+        int.from_bytes(
+            message_block[4 * i : 4 * i + message_block_length], byteorder="little"
+        )
+        for i in range(len(message_block) // message_block_length)
+    ]
 
 
 def message_to_hex(message_words: List[str]) -> List[int]:
@@ -45,11 +58,14 @@ def message_to_hex(message_words: List[str]) -> List[int]:
 def F(x, y, z):
     return (x & y) | (~x & z)
 
+
 def G(x, y, z):
-    return (x & y) | (y & ~z)
+    return (x & z) | (y & ~z)
+
 
 def H(x, y, z):
     return x ^ y ^ z
+
 
 def I(x, y, z):
     return y ^ (x | ~z)
@@ -58,90 +74,99 @@ def I(x, y, z):
 if __name__ == "__main__":
     text = "They are deterministic"
     assert len(text) <= 56  # 448/8
-    
-    msg_in_bytes = bytearray(text, 'ascii')
-    apply_padding(msg_in_bytes)
-    
-    
-    
 
-    # message_block = text_to_binary_message(text)
-    # padded_message_block = apply_padding(message_block)
+    msg_in_bytes = bytearray(text, "ascii")
+    padded_message = apply_padding(msg_in_bytes)
 
-    # message_words = split_message_block(padded_message_block)
+    # message_words = int.from_bytes(block[4*G : 4*G + 4], byteorder='little')
+
+    message_words = split_message_block(padded_message)
+    # print(len(message_words))
     # message_words = [i[::-1] for i in message_words]
     # print(message_words)
-    
+
     # message_words_hex = message_to_hex(message_words)
-    
-    # k = [int(hex(floor(abs(sin(i))*(pow(2, 32)))), 16) for i in range(1, 65)]
-    
-    # # print([[m, hex(m), int(hex(m), 16)] for m in message_words_hex])
-    
-    
+
+    k = [int(hex(floor(abs(sin(i)) * (pow(2, 32)))), 16) for i in range(1, 65)]
+
     # # ans = F(B, C, D)
     # # print(hex(ans))
-    
+
     # # ans2 = modular_addition(A, ans)
     # # ans3 = modular_addition(message_words_hex[0], ans2)
-    
+
     # # ans4 = modular_addition(k[0], ans3)
-    
+
     # # ans5 = hex_32_bit_shift_left(ans4, 7)
-    
+
     # # ans6 = modular_addition(B, ans5)
     # # print(hex(ans6))
     # # print(bin(message_words_hex[0]), hex(message_words_hex[0]))
-    # for i in range(64):
-        
-    #     # Round 1
-    #     if 0 <= i < 16:
-    #         p = F(B, C, D)
-    #         m_i = i
-    #         shift = [7, 12, 17, 22]
-    #         # print(hex(p))
-            
-    #     # Round 2
-    #     elif 16 <= i < 32:
-    #         p = G(B, C, D)
-    #         m_i = ((5 * i) + 1) % 16
-    #         shift = [5, 9, 14, 20]
-            
-    #     # Round 3
-    #     elif 32 <= i < 48:
-    #         p = H(B, C, D)
-    #         m_i = ((3 * i) + 5) % 16
-    #         shift = [4, 11, 16, 23]
-            
-    #     # Round 4
-    #     elif 48 <= i < 64:
-    #         p = I(B, C, D)
-    #         m_i = (7 * i) % 16
-    #         shift = [6, 10, 15, 21]
-        
-    #     print()
-    #     print(A, p, k[i], message_words_hex[i])#, int.from_bytes(message_to_hex[4*G : 4*G + 4], byteorder='little'))
-        
-        
-    #     # j = (k[i] + A + p + message_words_hex[m_i]) & 0xffffffff
-    #     j = A + p + k[i] + int.from_bytes(bytearray.fromhex(hex(message_words_hex[i])[2:]), byteorder='little')
-    #     print(j)
-    #     # p = modular_addition(k[i], p)
-        
-    #     # p = modular_addition(A, p)
+    for i in range(64):
+        # Round 1
+        if 0 <= i < 16:
+            p = F(B, C, D)
+            m_i = i
+            shift = [7, 12, 17, 22]
 
-    #     # p = modular_addition(message_words_hex[m_i], p)
-        
-    #     p = hex_32_bit_shift_left(p, shift[i % 4])
-        
-    #     p = modular_addition(B, p)
+        # Round 2
+        elif 16 <= i < 32:
+            p = G(B, C, D)
+            m_i = ((5 * i) + 1) % 16
+            shift = [5, 9, 14, 20]
 
-    #     A = D
-    #     B = p
-    #     D = C
-    #     C = B
-            
-    #     # if (i + 1) % 16 == 0 : 
-    #     #     print(i, hex(A), hex(B), hex(C), hex(D))  
-            
-    # # print(hex(A), hex(B), hex(C), hex(D))  
+        # Round 3
+        elif 32 <= i < 48:
+            p = H(B, C, D)
+            m_i = ((3 * i) + 5) % 16
+            shift = [4, 11, 16, 23]
+
+        # Round 4
+        elif 48 <= i < 64:
+            p = I(B, C, D)
+            m_i = (7 * i) % 16
+            shift = [6, 10, 15, 21]
+
+        # print()
+        # print(A, p, k[i], message_words_hex[i])#, int.from_bytes(message_to_hex[4*G : 4*G + 4], byteorder='little'))
+
+        # j = (k[i] + A + p + message_words_hex[m_i]) & 0xffffffff
+        j = A + p + k[i] + message_words[m_i]
+        # print(A ,p ,k[i] ,message_words[m_i])
+        # print(f'j: {j}, shift {shift[i % 4]}')
+        # p = modular_addition(k[i], p)
+
+        # p = modular_addition(A, p)
+
+        # p = modular_addition(message_words_hex[m_i], p)
+        print()
+        after_shift = B + left_shift(j, shift[i % 4])
+
+        # 0xFFFFFFFF masks the variable, leaving only the last 8 bits and ignoring the remaining
+        final_new_b = after_shift & 0xFFFFFFFF
+        # p = modular_addition(B, p)
+
+        A, B, C, D = D, final_new_b, B, C
+
+        # if (i + 1) % 16 == 0 :
+        #     print(i, hex(A), hex(B), hex(C), hex(D))
+
+    final_A = (A + initial_A) & 0xFFFFFFFF
+    final_B = (B + initial_B) & 0xFFFFFFFF
+    final_C = (C + initial_C) & 0xFFFFFFFF
+    final_D = (D + initial_D) & 0xFFFFFFFF
+
+    # print(final_A, final_B, final_C, final_D)
+    # print('LAST', final_A << (32*0))
+    # print('LAST', final_B << (32*1))
+    # print('LAST', final_C << (32*2))
+    # print('LAST', final_D << (32*3))
+
+    digest = sum(
+        buffer_content << (32 * i)
+        for i, buffer_content in enumerate([final_A, final_B, final_C, final_D])
+    )
+
+    raw = digest.to_bytes(16, byteorder="little").hex()
+    print(raw)
+    # print('{:032x}'.format(int.from_bytes(raw, byteorder='big')))
--- hashing/utils.py
+++ hashing/utils.py
@@ -1,6 +1,5 @@
 Z = 0x100000000
 
-    
 
 def get_bit_count(binary: str) -> int:
     return len(format(binary, "b"))
@@ -20,8 +19,8 @@ def modular_addition(x, y):
     return (x + y) % Z
 
 
-x = '101011110100110000100111110000'
-y = '00101011110100110000100111110000'
+x = "101011110100110000100111110000"
+y = "00101011110100110000100111110000"
 
 x_bin = 0b101011110100110000100111110000
 y_bin = 0b00101011110100110000100111110000
@@ -29,17 +28,23 @@ y_bin = 0b00101011110100110000100111110000
 # print(bin(y_bin << 9))
 
 
-def hex_32_bit_shift_left(hex_num: int, shift: int) -> int:
-    binary = str(format(hex_num, "b")).zfill(32)
-    if len(binary) < 32:
-        binary = binary.zfill(32)
-    return binary_string_to_hex(binary[shift:] + binary[:shift])
-    
-    
+def left_shift(num: int, shift: int) -> int:
+    # binary = str(format(hex_num, "b")).zfill(32)
+    # if len(binary) < 32:
+    #     binary = binary.zfill(32)
+    # return binary_string_to_hex(binary[shift:] + binary[:shift])
+    num &= 0xFFFFFFFF
+    return (num << shift | num >> (32 - shift)) & 0xFFFFFFFF
+
+
+# print(left_shift(9945746635, 7))
+# # 1745249704
+
+
 def bytearray_to_binary_string(array: bytearray) -> str:
     def access_bit(data, num):
         base = int(num // 8)
         shift = int(num % 8)
         return (data[base] >> shift) & 0x1
-    
-    return ''.join([str(access_bit(array, i)) for i in range(len(array)*8)])
+
+    return "".join([str(access_bit(array, i)) for i in range(len(array) * 8)])

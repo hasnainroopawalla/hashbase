0d96d7893a7f22a91bb8330433578ca66c33734e
Hasnain Roopawalla <37022937+hasnainroopawalla@users.noreply.github.com>
2022-09-28 22:16:45 +0530

    MD5 (#1)
    * Initial message parsing
    
    * Create messages
    
    * Hex messages and initialization vectors
    
    * Round 1  - step 3, left shift utility
    
    * Convert input to bytearray
    
    * Working MD5, pending code cleanup
    
    * MD5 class and unittests
    
    * Cleaner md5 code
    
    * Working MD5 for large strings
    
    * Working MD5 with documentation

---
+++ hashing/utils.py
@@ -0,0 +1,27 @@
+Z = pow(2, 32)
+
+
+def left_shift(x: int, s: int) -> int:
+    """Circularly shift (rotate) x left by s bit positions.
+
+    Args:
+        x (int): The input integer.
+        s (int): The number of shifts (in bits).
+
+    Returns:
+        int: The left shifted value of the input integer.
+    """
+    return (x << s) | (x >> (32 - s))
+
+
+def modular_add(x: int, y: int) -> int:
+    """Performs modular addition of x and y modulo 2^32.
+
+    Args:
+        x (int): The first integer.
+        y (int): The second integer.
+
+    Returns:
+        int: The value obtained after modulo addition of x and y.
+    """
+    return (x + y) % Z

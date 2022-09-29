f4e799efb6a833d296a1b3b97ab837564532bac4
hasnainroopawalla <hasnain.roopawalla@gmail.com>
2022-09-29 14:35:41 +0530

    SHA-1 with tests

--- hashing/sha1.py
+++ hashing/sha1.py
@@ -1,12 +1,11 @@
-from math import floor, sin
 from typing import List
 
-from hashing.utils import left_shift, modular_add
+from hashing.utils import left_shift, modular_add, pad_message
 
 
 class SHA1:
-    """A cryptographic hashing function used to produce a 128-bit hash.
-    https://en.wikipedia.org/wiki/MD5
+    """A cryptographic hashing function used to produce a 160-bit hash.
+    https://en.wikipedia.org/wiki/SHA-1
     """
 
     def __init__(self) -> None:
@@ -17,56 +16,22 @@ class SHA1:
         self.h4: int = 0xC3D2E1F0
 
     @staticmethod
-    def apply_padding(message: bytearray) -> bytearray:
-        """Pre-processing for the input message.
-        Appends a trailing '1'.
-        Pad 0s to the message.
-        Append message length to the message.
+    def break_message_block_into_words(message_block: bytearray) -> List[int]:
+        """Split and extend the 64-byte message block into 80 4-byte words.
 
         Args:
-            message (bytearray): The input message in bytes.
+            message_block (bytearray): The 512-bytes message block.
 
         Returns:
-            bytearray: The pre-processed message in bytes.
+            List[int]: A List of 80 4-byte words created by splitting the message block.
         """
-        # Store the length of the message in bytes
-        message_length = len(message)
-
-        # Pad a trailing '1'
-        message.append(0x80)
-
-        # Pad 0s to assert a block length of 448 bits (56 bytes)
-        while len(message) % 64 != 56:
-            message.append(0)
-
-        # Pad the last 64 bits that indicate the message length in the little endian format
-        message += (message_length * 8).to_bytes(8, byteorder="big")
-
-        return message
-
-    @staticmethod
-    def break_message_block_into_words(message_block: bytearray) -> List[int]:
-        w = [0 for _ in range(80)]
+        w = list(range(80))
         for i in range(80):
             if 0 <= i < 16:
                 w[i] = int.from_bytes(message_block[4 * i : 4 * i + 4], byteorder="big")
-
             else:
                 w[i] = left_shift((w[i - 3] ^ w[i - 8] ^ w[i - 14] ^ w[i - 16]), 1)
-
         return w
-
-    @staticmethod
-    def F(x: int, y: int, z: int) -> int:
-        return (x & y) | (~x & z)
-
-    @staticmethod
-    def G(x: int, y: int, z: int) -> int:
-        return x ^ y ^ z
-
-    @staticmethod
-    def H(x: int, y: int, z: int) -> int:
-        return (x & y) | (x & z) | (y & z)
 
     def register_values_to_hex_string(self) -> str:
         """Read the values of the 5 registers and convert them to a hexadecimal string.
@@ -77,16 +42,16 @@ class SHA1:
         return "%08x%08x%08x%08x%08x" % (self.h0, self.h1, self.h2, self.h3, self.h4)
 
     def generate_hash(self, message: str) -> str:
-        """Generates a 128-bit MD5 hash of the input message.
+        """Generates a 160-bit SHA-1 hash of the input message.
 
         Args:
             message (str): The input message/text.
 
         Returns:
-            str: The 128-bit MD5 hash of the message.
+            str: The 160-bit SHA-1 hash of the message.
         """
         message_in_bytes = bytearray(message, "ascii")
-        message_chunk = self.apply_padding(message_in_bytes)
+        message_chunk = pad_message(message_in_bytes, "big")
 
         # Loop through each 64-byte message block
         for block in range(len(message_chunk) // 64):
@@ -96,26 +61,21 @@ class SHA1:
 
             a, b, c, d, e = self.h0, self.h1, self.h2, self.h3, self.h4
 
-            # 4 rounds of 16 operations
             for i in range(80):
-                # Round 1
                 if 0 <= i < 20:
-                    f = self.F(b, c, d)
+                    f = (b & c) | (~b & d)
                     k = 0x5A827999
 
-                # Round 2
                 elif 20 <= i < 40:
-                    f = self.G(b, c, d)
+                    f = b ^ c ^ d
                     k = 0x6ED9EBA1
 
-                # Round 3
                 elif 40 <= i < 60:
-                    f = self.H(b, c, d)
+                    f = (b & c) | (b & d) | (c & d)
                     k = 0x8F1BBCDC
 
-                # Round 4
                 elif 60 <= i < 80:
-                    f = self.G(b, c, d)
+                    f = b ^ c ^ d
                     k = 0xCA62C1D6
 
                 temp = modular_add(left_shift(a, 5), f)
@@ -136,6 +96,3 @@ class SHA1:
             self.h4 = modular_add(self.h4, e)
 
         return self.register_values_to_hex_string()
-
-
-print(SHA1().generate_hash("abc"))
--- hashing/utils.py
+++ hashing/utils.py
@@ -25,3 +25,32 @@ def modular_add(x: int, y: int) -> int:
         int: The value obtained after modulo addition of x and y.
     """
     return (x + y) % Z
+
+
+def pad_message(message: bytearray, message_length_byteorder: str) -> bytearray:
+    """Pre-processing for the input message.
+    Appends a trailing '1'.
+    Pad 0s to the message.
+    Append message length to the message in little or big endian.
+
+    Args:
+        message (bytearray): The input message in bytes.
+        message_length_byteorder (str): Can be either 'big' or 'little', indicating if the last 64 bits of the message (message length) are in the big or little endian convention.
+
+    Returns:
+        bytearray: The pre-processed message in bytes.
+    """
+    # Store the length of the message in bytes
+    message_length = len(message)
+
+    # Pad a trailing '1'
+    message.append(0x80)
+
+    # Pad 0s to assert a block length of 448 bits (56 bytes)
+    while len(message) % 64 != 56:
+        message.append(0)
+
+    # Pad the last 64 bits that indicate the message length in the specified endian format
+    message += (message_length * 8).to_bytes(8, byteorder=message_length_byteorder)
+
+    return message

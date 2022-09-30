d1d94eba32128002687cea069dbbe8273975c514
Hasnain Roopawalla <37022937+hasnainroopawalla@users.noreply.github.com>
2022-09-30 11:15:30 +0530

    MD2 (#3)
    * Initial MD2
    
    * MD2 Working with Tests
    
    * MD2 with tests
    
    * Actions workflow
    
    * Utility type hint fix
    
    * Action workflow update
    
    * Type hint fix
    
    * Add __init__.py
    
    * Add __init__.py

--- examples/hash.py
+++ examples/hash.py
@@ -1,7 +1,9 @@
+from hashing.md2 import MD2
 from hashing.md5 import MD5
 from hashing.sha1 import SHA1
 
-message: str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+message: str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"
 
+print(f"MD2: {MD2().generate_hash(message)}")
 print(f"MD5: {MD5().generate_hash(message)}")
 print(f"SHA-1: {SHA1().generate_hash(message)}")

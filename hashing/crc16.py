221271708016a79e47679dfcaa42992e6dd50ebb
hasnainroopawalla <hasnain.roopawalla@gmail.com>
2022-10-02 11:28:36 +0530

    Linting

--- hashing/crc16.py
+++ hashing/crc16.py
@@ -1,11 +1,13 @@
-class CRC16():
-    """This implementation of CRC-16 is inspired by https://sourceforge.net/projects/crccheck/
-    """
-    def __init__(self, poly: int = 0x8005, init_value: int = 0x0000, xor_out: int = 0x0000) -> None:
+class CRC16:
+    """This implementation of CRC-16 is inspired by https://sourceforge.net/projects/crccheck/"""
+
+    def __init__(
+        self, poly: int = 0x8005, init_value: int = 0x0000, xor_out: int = 0x0000
+    ) -> None:
         self.poly = poly
         self.init_value = init_value
         self.xor_out = xor_out
-        
+
     def generate_hash(self, message: str) -> str:
         """Generates a CRC-16 hash of the input message.
 
@@ -15,10 +17,10 @@ class CRC16():
         Returns:
             str: The CRC-16 hash of the message.
         """
-        message_bytes = bytearray(message, 'ascii')
+        message_bytes = bytearray(message, "ascii")
         crc = self.init_value
         for byte in message_bytes:
-            crc ^= (byte << 8)
+            crc ^= byte << 8
             for _ in range(8):
                 crc = (crc << 1) ^ self.poly if crc & 0x8000 else crc << 1
             crc &= 0xFFFF
--- hashing/crc8.py
+++ hashing/crc8.py
@@ -1,11 +1,13 @@
-class CRC8():
-    """This implementation of CRC-8 is inspired by https://sourceforge.net/projects/crccheck/
-    """
-    def __init__(self, poly: int = 0x07, init_value: int = 0x00, xor_out: int = 0x00) -> None:
+class CRC8:
+    """This implementation of CRC-8 is inspired by https://sourceforge.net/projects/crccheck/"""
+
+    def __init__(
+        self, poly: int = 0x07, init_value: int = 0x00, xor_out: int = 0x00
+    ) -> None:
         self.poly = poly
         self.init_value = init_value
         self.xor_out = xor_out
-        
+
     def generate_hash(self, message: str) -> str:
         """Generates a CRC-8 hash of the input message.
 
@@ -15,7 +17,7 @@ class CRC8():
         Returns:
             str: The CRC-8 hash of the message.
         """
-        message_bytes = bytearray(message, 'ascii')
+        message_bytes = bytearray(message, "ascii")
         crc = self.init_value
         for byte in message_bytes:
             crc ^= byte
@@ -23,4 +25,3 @@ class CRC8():
                 crc = (crc << 1) ^ self.poly if crc & 0x80 else crc << 1
             crc &= 0xFF
         return hex(crc ^ self.xor_out)
-

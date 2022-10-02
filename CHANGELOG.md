5f83458aa78c24e3fb41fa1f9aad868bc2908025
hasnainroopawalla <hasnain.roopawalla@gmail.com>
2022-10-02 11:51:07 +0530

    Packaging tools and added a CHANGELOG

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Change Log
+
+## 02-Oct-2022 [1.0.0]
+- Added the following hash functions:
+  - MD2 (`hashbase.MD2`)
+  - MD4 (`hashbase.MD4`)
+  - MD5 (`hashbase.MD5`)
+  - SHA-1 (`hashbase.SHA1`)
+  - CRC-8 (`hashbase.CRC8`)
+  - CRC-16 (`hashbase.CRC16`)
+- Added unit tests for all hash functions
+- Added an example file `examples/hash.py` that calls all the implemented hash functions
--- examples/hash.py
+++ examples/hash.py
@@ -1,4 +1,4 @@
-from hashbase import MD2, MD4, MD5, SHA1
+from hashbase import MD2, MD4, MD5, SHA1, CRC8, CRC16
 
 
 message: str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"
@@ -7,3 +7,5 @@ print(f"MD2: {MD2().generate_hash(message)}")
 print(f"MD4: {MD4().generate_hash(message)}")
 print(f"MD5: {MD5().generate_hash(message)}")
 print(f"SHA-1: {SHA1().generate_hash(message)}")
+print(f"CRC-8: {CRC8().generate_hash(message)}")
+print(f"CRC-16: {CRC16().generate_hash(message)}")

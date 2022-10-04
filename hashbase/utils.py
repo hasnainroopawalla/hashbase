71d4caa375eeb3fe85f21911b3ebb0398efaeea6
hasnainroopawalla <hasnain.roopawalla@gmail.com>
2022-10-04 09:12:31 +0530

    message padding utility update

--- hashbase/utils.py
+++ hashbase/utils.py
@@ -69,7 +69,7 @@ def modular_add(nums: List[int]) -> int:
 
 
 def apply_message_padding(
-    message: bytearray, message_length_byteorder: Literal["little", "big"]
+    message: bytearray, message_length_byteorder: Literal["little", "big"], message_length_padding_bits: int = 64, message_chunk_size_bits: int = 512
 ) -> bytearray:
     """Pre-processing for the input message.
     Appends a trailing '1'.
@@ -79,21 +79,23 @@ def apply_message_padding(
     Args:
         message (bytearray): The input message in bytes.
         message_length_byteorder (str): Can be either 'big' or 'little', indicating if the last 64 bits of the message (message length) are in the big or little endian convention.
+        message_length_padding_bits (int): The number of bits to be appended at the end of the message chunk to indicate the length of the original message.
+        message_chunk_size_bits (int): The size of the message chunk in bits.
 
     Returns:
         bytearray: The pre-processed message in bytes.
     """
-    # Store the length of the message in bytes
-    message_length = len(message)
+    # Store the length of the message in bits
+    original_message_length_in_bits = len(message) * 8
 
     # Pad a trailing '1'
     message.append(0x80)
-
-    # Pad 0s to assert a block length of 448 bits (56 bytes)
-    while len(message) % 64 != 56:
+    
+    # Pad 0s to assert a block length of (message_chunk_size_bits-message_length_padding_bits) bits
+    while (len(message) * 8 + message_length_padding_bits) % message_chunk_size_bits != 0:
         message.append(0)
 
-    # Pad the last 64 bits that indicate the message length in the specified endian format
-    message += (message_length * 8).to_bytes(8, byteorder=message_length_byteorder)
+    # Pad the last message_length_padding_bits bits that indicate the message length in the specified endian format
+    message += (original_message_length_in_bits).to_bytes(message_length_padding_bits//8, byteorder=message_length_byteorder)
 
     return message
